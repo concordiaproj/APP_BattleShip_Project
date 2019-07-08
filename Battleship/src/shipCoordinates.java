817fcd8d1a605118635f43f9c9b1ea61c479a29b
Dhvani Agola <ddhvani1994@gmail.com>
2019-07-08 13:27:32 -0400

    First Commit Remaining : - remove code for leaving space between any two ships - refactoring

---
+++ Battleship/src/shipCoordinates.java
@@ -0,0 +1,13 @@
+
+public class shipCoordinates {
+
+	int Xcoord;
+	int Ycoord;
+
+	public shipCoordinates(int Xcoord, int Ycoord) {
+		// TODO Auto-generated constructor stub//
+		this.Xcoord = Xcoord;
+		this.Ycoord = Ycoord;
+	}
+
+}
