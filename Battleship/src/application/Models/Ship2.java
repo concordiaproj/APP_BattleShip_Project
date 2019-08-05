9ce2ec280bb74bc5d3a01af95fd3f8eee7db9914
Dhvani Agola <ddhvani1994@gmail.com>
2019-08-05 12:35:53 -0400

    commit

--- Battleship/src/application/Models/Ship2.java
+++ Battleship/src/application/Models/Ship2.java
@@ -11,30 +11,45 @@ public class Ship2 {
 	int intEndX;
 	int intEndY;
 	boolean boolIsHit;
-	List<DataCoordinates> lstShipCoord=new ArrayList<>();
+	List<DataCoordinates> lstShipCoord = new ArrayList<>();
+
 //	String strShipId;
-	public Ship2(int intSize,int intStartX,int intStartY,int intEndX,int intEndY,boolean boolIsHit) {
-		this.intSize=intSize;
-		this.intStartX=intStartX;
-		this.intStartY=intStartY;
-		this.intEndX=intEndX;
-		this.intEndY=intEndY;
-		this.boolIsHit=boolIsHit;
+	/**
+	 * This constructor set the information of a ship
+	 * 
+	 * @param intSize   size of the ship
+	 * @param intStartX x-Coordinate of first block of the ship
+	 * @param intStartY y-Coordinate of first block of the ship
+	 * @param intEndX   x-Coordinate of last block of the ship
+	 * @param intEndY   y-Coordinate of last block of the ship
+	 * @param boolIsHit Is whole ship is hit or not
+	 */
+	public Ship2(int intSize, int intStartX, int intStartY, int intEndX, int intEndY, boolean boolIsHit) {
+		this.intSize = intSize;
+		this.intStartX = intStartX;
+		this.intStartY = intStartY;
+		this.intEndX = intEndX;
+		this.intEndY = intEndY;
+		this.boolIsHit = boolIsHit;
 //		this.strShipId=strShipId;
 	}
-	
+
 	public int getSX() {
 		return intStartX;
 	}
+
 	public int getSY() {
 		return intStartY;
 	}
+
 	public int getEX() {
 		return intEndX;
 	}
+
 	public int getEY() {
 		return intEndY;
 	}
+
 	public int getSizeOfShip() {
 		return intSize;
 	}
@@ -46,24 +61,26 @@ public class Ship2 {
 	public void setBoolIsHit(boolean boolIsHit) {
 		this.boolIsHit = boolIsHit;
 	}
-	// Ship Co-ordation Method
-	 public void setShipCoord() {
-			// TODO Auto-generated method stub
-	    	int x=this.getSX();
-	    	int y=this.getSY();
-	    	int xEnd=this.getEX();
-	    	int yEnd=this.getEY();
-	    	if(x==xEnd) {
-	    		for(int i=y;i<=yEnd;i++) {
-	    			DataCoordinates dc=new DataCoordinates(x, i);
-	    			this.lstShipCoord.add(dc);
-	    		}
-	    	}
-	    	else {
-	    		for(int j=x;j<=xEnd;j++) {
-	    			DataCoordinates dc=new DataCoordinates(j,y);
-	    			this.lstShipCoord.add(dc);
-	    		}
-	    	}
+
+	/**
+	 * This method will set the coordinates of all the blocks of a ship
+	 */
+	public void setShipCoord() {
+		// TODO Auto-generated method stub
+		int x = this.getSX();
+		int y = this.getSY();
+		int xEnd = this.getEX();
+		int yEnd = this.getEY();
+		if (x == xEnd) {
+			for (int i = y; i <= yEnd; i++) {
+				DataCoordinates dc = new DataCoordinates(x, i);
+				this.lstShipCoord.add(dc);
+			}
+		} else {
+			for (int j = x; j <= xEnd; j++) {
+				DataCoordinates dc = new DataCoordinates(j, y);
+				this.lstShipCoord.add(dc);
+			}
 		}
+	}
 }
