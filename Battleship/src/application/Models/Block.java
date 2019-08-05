b7b0e31a80d3dd3f467a1b4571958c4a9b7259e8
Dhvani Agola <ddhvani1994@gmail.com>
2019-08-05 12:34:49 -0400

    commit

--- Battleship/src/application/Models/Block.java
+++ Battleship/src/application/Models/Block.java
@@ -1,25 +1,44 @@
 package application.Models;
 
+/**
+ * 
+ * 
+ * @author dhvaniagola
+ * @author arunipatel
+ * @author SAHIL
+ */
 public class Block {
-private char charOccupiedFor;
-private int x;
-private int y;
-private Ship2 ship;
-public Block(int x,int y,char charOccupiedFor) {
-	this.charOccupiedFor=charOccupiedFor;
-	this.x=x;
-	this.y=y;
-}
-public void setShip(Ship2 ship) {
-	this.ship=ship;
-}
-public Ship2 getShip() {
-	return this.ship;
-}
-public void setCharOccupiedFor(char c) {
-	this.charOccupiedFor=c;
-}
-public char getCharOccupiedFor() {
-	return this.charOccupiedFor;
-}
+	private char charOccupiedFor;
+	private int x;
+	private int y;
+	private Ship2 ship;
+
+	/**
+	 * This constructor set the information of each block of the grid
+	 * 
+	 * @param x               x-Coordinate of block
+	 * @param y               y-Coordinate of block
+	 * @param charOccupiedFor Char which define the state of the block
+	 */
+	public Block(int x, int y, char charOccupiedFor) {
+		this.charOccupiedFor = charOccupiedFor;
+		this.x = x;
+		this.y = y;
+	}
+
+	public void setShip(Ship2 ship) {
+		this.ship = ship;
+	}
+
+	public Ship2 getShip() {
+		return this.ship;
+	}
+
+	public void setCharOccupiedFor(char c) {
+		this.charOccupiedFor = c;
+	}
+
+	public char getCharOccupiedFor() {
+		return this.charOccupiedFor;
+	}
 }
